9c655f10476858cd2b0f2ce8bc0cf1e968c2f9aa
Winlentia <winlentia@gmail.com>
2022-08-12 00:28:32 +0300

    .gitignore added to repo and accepted

--- Pods/Target Support Files/Pods-CaseMovie-CaseMovieUITests/Pods-CaseMovie-CaseMovieUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CaseMovie_CaseMovieUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CaseMovie_CaseMovieUITests
-@end
--- Pods/Target Support Files/Pods-CaseMovie/Pods-CaseMovie-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CaseMovie : NSObject
-@end
-@implementation PodsDummy_Pods_CaseMovie
-@end
